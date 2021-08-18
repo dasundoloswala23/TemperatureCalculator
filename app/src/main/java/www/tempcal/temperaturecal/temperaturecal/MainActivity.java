565d7dc5450a83fa0de6906ac905785164c324cb
Tharindu Dasun <tharindudasun0@gmail.com>
2021-08-18 18:30:28 +0530

    Temperature Calculator

---
+++ app/src/main/java/www/tempcal/temperaturecal/temperaturecal/MainActivity.java
@@ -0,0 +1,69 @@
+package www.tempcal.temperaturecal.temperaturecal;
+
+import static java.lang.Double.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    Button ansBtn, againBtn;
+    TextView showAns;
+    EditText getVal;
+    RadioButton c2f, f2c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ansBtn = (Button) findViewById(R.id.ansBtn);
+        showAns = (TextView) findViewById(R.id.showAns);
+        getVal = (EditText) findViewById(R.id.getVal);
+        c2f = (RadioButton) findViewById(R.id.c2f);
+        f2c = (RadioButton) findViewById(R.id.f2c);
+        againBtn = (Button) findViewById(R.id.againBtn);
+        ansBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getVal.getText().toString().isEmpty()) {
+                    showAns.setText("Please give Temperature");
+
+                } else {
+                    double x = parseDouble(String.valueOf(getVal.getText()));
+                    if (c2f.isChecked()) {
+                        x = (x * 9) / 5 + 32;
+                        // this is Equation
+                        x = Double.parseDouble(new DecimalFormat("##.###").format(x));
+                        showAns.setText(String.valueOf(x) + " Degree F ");
+                    } else if (f2c.isChecked()) {
+                        x = (x - 32) * 5 / 9;
+                        x = Double.parseDouble(new DecimalFormat("##.###").format(x));
+                        showAns.setText(String.valueOf(x) + " Degree C ");
+                    } else {
+                        showAns.setText("Please Select an option !");
+                    }
+                }
+
+            }
+
+
+        });
+        againBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAns.setText("0.0");
+                getVal.setText("");
+                c2f.setChecked(false);
+                f2c.setChecked(false);
+            }
+        });
+
+    }
+};
